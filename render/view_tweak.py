ae12b8c3fbf89436386d613757c412056a219cee
Samuel Bernou <bernou.samuel@gmail.com>
2017-11-20 15:08:42 +0100

    add material switch mode operator

--- render/view_tweak.py
+++ render/view_tweak.py
@@ -13,10 +13,10 @@ class SAMT_VIEW3D_OT_show_only_render(bpy.types.Operator):
     bl_label = "Show Only Render"
 
     def execute(self, context):
-        
+
         space = bpy.context.space_data
         space.show_only_render = not space.show_only_render
-        
+
         return {"FINISHED"}
 
 
@@ -29,9 +29,25 @@ class SAMT_VIEW3D_OT_lock_camera_to_view(bpy.types.Operator):
     bl_label = "Lock camera to view"
 
     def execute(self, context):
-        
+
         #TODO (only if in view is in cam else, detect view)
         space = bpy.context.space_data
         space.lock_camera = not space.lock_camera
-        
+
+        return {"FINISHED"}
+
+#Ctrl+Alt+Shift+Z (all classic modifiers) go material
+
+class SAMT_VIEW3D_OT_material_mode(bpy.types.Operator):
+    bl_idname = "samutils.material_mode"
+    bl_label = "Go To material mode"
+
+    def execute(self, context):
+
+        space = bpy.context.space_data
+        if space.viewport_shade == 'MATERIAL':
+            space.viewport_shade = 'SOLID'
+        else:
+            space.viewport_shade = 'MATERIAL'
+
         return {"FINISHED"}
